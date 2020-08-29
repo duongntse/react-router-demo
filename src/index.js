dc02a4e1b60728de26144a04f3e657b58088027b
Harry Nguyen <duongntse@gmail.com>
2020-08-29 16:09:59 +0700

    paused react-snapshot codes

--- src/index.js
+++ src/index.js
@@ -7,23 +7,18 @@ import * as serviceWorker from './serviceWorker';
 
 const { run } = require('react-snap');
 
-(async () =>
-	await run({
-		// You would have other args here,
-		puppeteerArgs: ['--no-sandbox', '--disable-setuid-sandbox'],
-	}))();
-
 const rootElement = document.getElementById('root');
-if (rootElement.hasChildNodes()) {
-	ReactDOM.hydrate(<Root />, rootElement);
-} else {
-	ReactDOM.render(<Root />, rootElement);
-}
-// ReactDOM.render(
-// 	<React.StrictMode>
-// 		<Root />
-// 	</React.StrictMode>,
-// );
+// if (rootElement.hasChildNodes()) {
+// 	ReactDOM.hydrate(<Root />, rootElement);
+// } else {
+// 	ReactDOM.render(<Root />, rootElement);
+// }
+ReactDOM.render(
+	<React.StrictMode>
+		<Root />
+	</React.StrictMode>,
+	rootElement
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
